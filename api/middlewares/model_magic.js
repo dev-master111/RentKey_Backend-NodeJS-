aadbd6ba2ac5edf1c3916d509e5967a45e50ffda
Happyboy171 <luckyboy1992330@mail.com>
2017-02-27 22:05:37 +0800

    Add a model magic midleware for APIs.

---
+++ api/middlewares/model_magic.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+export default function (model, param = 'id') {
+    let m = mongoose.model(model);
+    return async function (req, res, next) {
+        if (!mongoose.Types.ObjectId.isValid(req.params[param])) {
+            return next();
+        }
+        let found;
+        try {
+            found = await m.findById(req.params[param]);
+        } catch (e) {
+            return res.error('Could not query model');
+        }
+        if (!found) {
+            return res.error(`${capitalize(model)} not found.`, 404);
+        }
+        if (!req[model]) {
+            req[model] = found;
+        }
+        next();
+    };
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}

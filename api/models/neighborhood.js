67a62ab3a57927463586731c02296f761e166ca1
Happyboy171 <luckyboy1992330@mail.com>
2017-02-28 22:28:00 +0800

    Add a neighbourhood model for management.

---
+++ api/models/neighborhood.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+import P from 'bluebird';
+import timestamps from 'mongoose-timestamp';
+import update from 'mongoose-model-update';
+
+/**
+ * Neighborhood Schema
+ */
+
+let Neighborhood = new Schema({
+    name: {
+        type: String,
+        required: 'Neighborhood name is required.'
+    },
+    lat: {
+        type: Number,
+        default: 0.0
+    },
+    lng: {
+        type: Number,
+        default: 0.0
+    },
+    coordinates: [{
+        type: String
+    }]
+});
+
+Neighborhood.plugin(timestamps);
+Neighborhood.plugin(update, ['name', 'coordinates']);
+
+Neighborhood.statics.isValidId = function (neighborhoodId) {
+};
+
+module.exports = mongoose.model('Neighborhood', Neighborhood);

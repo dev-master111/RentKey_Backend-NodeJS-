2756f954e275659ce21cbfd416cac2473ddf3621
Happyboy171 <luckyboy1992330@mail.com>
2017-03-01 23:18:49 +0800

    Add config file for project.

---
+++ config/config.js
@@ -0,0 +1,34 @@
+﻿import path from 'path';
+let rootPath = path.normalize(__dirname + '/..');
+import plaid from 'plaid';
+
+module.exports = {
+    development: {
+        db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/rentkey_dev',
+        root: rootPath,
+        app: {
+            name: 'RentKey Backend'
+        },
+        secret: 'secretkeyforrentkeywebservice',
+        admin: {
+            email: 'admin@rentkey.com',
+            password: 'pass'
+        },
+        avatarImagesFolder: 'uploads/avatars',
+        propertyImagesFolder: 'uploads/properties'
+    },
+    production: {
+        db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/rentkey_prod',
+        root: rootPath,
+        app: {
+            name: 'Rentkey Backend'
+        },
+        secret: 'secretkeyforrentkeywebservice',
+        admin: {
+            email: 'admin@rentkey.com',
+            password: 'pass'
+        },
+        avatarImagesFolder: 'uploads/avatars',
+        propertyImagesFolder: 'uploads/properties'
+    }
+};
